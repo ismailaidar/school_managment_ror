76a2e7fbd6cce3428e1092a9f4248bc567ea1dd7
ismailaidar <ismailaidar@gmail.com>
2020-07-24 20:57:35 -0400

    fix relationshipt between subject & level

--- db/migrate/20200725001027_create_subjects.rb
+++ db/migrate/20200725001027_create_subjects.rb
@@ -2,7 +2,7 @@ class CreateSubjects < ActiveRecord::Migration[6.0]
   def change
     create_table :subjects do |t|
       t.string :name
-      t.references :level_id
+      t.references :level, null: false, foreign_key: true
 
       t.timestamps
     end
